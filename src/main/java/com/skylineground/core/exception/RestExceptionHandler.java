6159b15b15d345d2ff468089d4910adf7643dc90
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-02-26 20:46:18 -0300

    feat: add handle to EmailAlreadyExistsException in RestExceptionHandler (#14)

--- src/main/java/com/skylineground/core/exception/RestExceptionHandler.java
+++ src/main/java/com/skylineground/core/exception/RestExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.skylineground.core.exception;
 
 import com.skylineground.core.constant.AppErrorCode;
+import com.skylineground.modules.identity.user.EmailAlreadyExistsException;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,11 @@ public class RestExceptionHandler {
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ProblemDetail handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
         return buildProblemDetail(AppErrorCode.INVALID_JSON);
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ProblemDetail handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
+        return buildProblemDetail(ex.getErrorCode());
     }
 
     private ProblemDetail buildProblemDetail(AppErrorCode errorCode) {
