f2a2210470fbb7f6f5377efe26a18aefaafe9f80
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-02-26 20:10:33 -0300

    feat: add handle to invalid JSON in RestExceptionHandler (close #13)

--- src/main/java/com/skylineground/core/exception/RestExceptionHandler.java
+++ src/main/java/com/skylineground/core/exception/RestExceptionHandler.java
@@ -2,10 +2,17 @@ package com.skylineground.core.exception;
 
 import com.skylineground.core.constant.AppErrorCode;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ProblemDetail handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return buildProblemDetail(AppErrorCode.INVALID_JSON);
+    }
 
     private ProblemDetail buildProblemDetail(AppErrorCode errorCode) {
         var problemDetail = ProblemDetail.forStatus(errorCode.getHttpStatus());
