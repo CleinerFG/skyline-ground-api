fabc78f45f47b4b56b07dae15f5d73278a3dc30c
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-02-26 02:47:28 -0300

    feat: initial commit with project files

---
+++ src/main/java/com/skylineground/SkylineGroundApplication.java
@@ -0,0 +1,13 @@
+package com.skylineground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkylineGroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkylineGroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skylineground/SkylineGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skylineground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkylineGroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
