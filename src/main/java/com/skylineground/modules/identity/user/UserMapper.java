1473ea3a18446515fb5746b427fe31587a759c14
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-02-26 18:38:48 -0300

    feat(user): create mapper

---
+++ src/main/java/com/skylineground/modules/identity/user/UserMapper.java
@@ -0,0 +1,24 @@
+package com.skylineground.modules.identity.user;
+
+import com.skylineground.modules.identity.user.dto.UserRequest;
+import com.skylineground.modules.identity.user.dto.UserResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public User toEntity(UserRequest dto) {
+        return User.builder()
+                .name(dto.name())
+                .email(dto.email())
+                .password(dto.password())
+                .build();
+    }
+
+    public UserResponse toResponse(User entity) {
+        return UserResponse.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .email(entity.getEmail())
+                .build();
+    }
+}
