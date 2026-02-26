ac63a11d353e16a535c15c6220a3e506e74ddf41
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-02-26 18:39:46 -0300

    feat(user): create service

---
+++ src/main/java/com/skylineground/modules/identity/user/UserService.java
@@ -0,0 +1,27 @@
+package com.skylineground.modules.identity.user;
+
+import com.skylineground.modules.identity.user.dto.UserRequest;
+import com.skylineground.modules.identity.user.dto.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserResponse create(UserRequest dto) {
+        var user = userMapper.toEntity(dto);
+
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        var savedUser = userRepository.save(user);
+
+        return userMapper.toResponse(savedUser);
+    }
+}
