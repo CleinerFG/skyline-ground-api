abbeadf1f0dca282da4d65d510c2870e495b4f9c
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-02-27 00:50:19 -0300

    feat(auth): implement LoginService for user authentication (close #19)
    Introduces LoginService to handle the authentication process by validating user credentials via AuthenticationManager and generating a JWT token using TokenService.

---
+++ src/main/java/com/skylineground/modules/identity/auth/LoginService.java
@@ -0,0 +1,37 @@
+package com.skylineground.modules.identity.auth;
+
+import com.skylineground.core.security.TokenService;
+import com.skylineground.modules.identity.auth.dto.LoginRequest;
+import com.skylineground.modules.identity.auth.dto.TokenResponse;
+import com.skylineground.modules.identity.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final TokenService tokenService;
+    private final AuthenticationManager authenticationManager;
+
+    public TokenResponse authenticate(LoginRequest dto) {
+        try {
+            var authToken = new UsernamePasswordAuthenticationToken(
+                    dto.email(),
+                    dto.password()
+            );
+            Authentication authentication = authenticationManager.authenticate(authToken);
+
+            User user = (User) authentication.getPrincipal();
+            String token = tokenService.generate(user);
+
+            return new TokenResponse(token);
+        } catch ( BadCredentialsException e ) {
+            throw new InvalidCredentialsException();
+        }
+    }
+}
