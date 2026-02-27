a2d983a2aa81a64a9146169a1de2c5f5f882758a
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-02-27 17:58:06 -0300

    fix(security): remove "api" prefix from requestMatchers (fix: #34)

--- src/main/java/com/skylineground/core/security/SecurityConfig.java
+++ src/main/java/com/skylineground/core/security/SecurityConfig.java
@@ -31,8 +31,8 @@ public class SecurityConfig {
                         SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
