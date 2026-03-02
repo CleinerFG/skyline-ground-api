3f5bb9dfc704f6f9a9288b0b011bb580e03cd528
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-03-02 02:54:18 -0300

    feat: add AUTHENTICATION_REQUIRED and SYSTEM_INTEGRITY_VIOLATION to GlobalErrorCode (#46)

--- src/main/java/com/skylineground/core/exception/GlobalErrorCode.java
+++ src/main/java/com/skylineground/core/exception/GlobalErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum GlobalErrorCode implements ErrorCode {
 
-    INVALID_JSON("INVALID_JSON", HttpStatus.BAD_REQUEST);
+    INVALID_JSON("INVALID_JSON", HttpStatus.BAD_REQUEST),
+    AUTHENTICATION_REQUIRED("AUTHENTICATION_REQUIRED", HttpStatus.UNAUTHORIZED),
+    SYSTEM_INTEGRITY_VIOLATION("SYSTEM_INTEGRITY_VIOLATION", HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final String code;
     private final HttpStatus httpStatus;
